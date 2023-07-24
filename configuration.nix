3ba6ce87d05d6ced4737c03c36d6bbf8920e2e56
xeylou <xeylou.pro@gmail.com>
2023-07-24 10:50:12 +0200

    working tpm settings for w11 in virt-manager :)

--- configuration.nix
+++ configuration.nix
@@ -27,19 +27,30 @@
     efi.canTouchEfiVariables = true;
   };
 
+  # enabling tpm for w11 virt
+  security.tpm2 = {
+    enable = true;
+    pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+    tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+  };
+
   # network configuration
   networking = {
     hostName = "null";
     networkmanager.enable = true;
+    extraHosts = 
+    ''
+      127.0.0.1 xeylou.fr
+    '';
     # wireless.enable = true;  # wireless support via wpa_supplicant
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-    nftables.enable = true;
+    # nftables.enable = true;
     
     # firewall = {
-      # enable = true;
-      # allowedTCPPorts = [ ... ];
-  # allowedUDPPorts = [ ... ];
+    #    enable = true;
+    #   allowedTCPPorts = [ ... ];
+    #   allowedUDPPorts = [ ... ];
     # };
   };
 
@@ -58,18 +69,20 @@
           LC_PAPER = "en_US.UTF-8";
           LC_TELEPHONE = "en_US.UTF-8";
           LC_TIME = "en_US.UTF-8";
-    }; };
-  # w11 windowing system
+    };
+  };
+
+  # x11 windowing system
   services.xserver.enable = true;
 
   # xfce
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
-  # if you want gnome
+  # example if you want gnome
   #services.xserver.displayManager.gdm.enable = true;
   #services.xserver.desktopManager.gnome.enable = true;
 
-  # keymap in X11
+  # keymap in x11
   services.xserver = {
     layout = "fr";
     xkbVariant = "";
@@ -111,17 +124,20 @@
   users.users.xeylou = {
     isNormalUser = true;
     description = "xeylou";
-    extraGroups = [ "networkmanager" "wheel" "kvm" "libvirtd" "audio" ];
+    #  tss for tpm
+    extraGroups = [ "networkmanager" "wheel" "kvm" "libvirtd" "audio" "tss" ];
     packages = with pkgs; [
 
       # software
       brave
+      mullvad-browser
       keepassxc
       discord
       vlc
 
       # networking related
       macchanger
+      wireguard-tools
       wireshark
       openvpn
       qbittorrent
@@ -137,11 +153,13 @@
       gnupg
       pinentry  # gnupg dependency
       # wget  nix-shell -p
+      xmousepasteblock # disable middle clic pasting
 
       # virtualisation
       qemu
       virt-manager
       dconf  # virt-manager dependency
+      # swtpm # tpm emulation
 
       # ide & dev
       tmux
@@ -150,16 +168,42 @@
       vscodium
       git
       python3
+      go
+      hugo
 
     ];
   };
 
   # packages installed in system profile
-  environment.systemPackages = with pkgs; [ virt-manager pkgs.wireguard-tools
+  environment.systemPackages = with pkgs; [
   ];
 
+  virtualisation.libvirtd = {
+    enable = true;
+
+    onShutdown = "suspend";
+    onBoot = "ignore";
+
+    qemu = {
+      package = pkgs.qemu_kvm;
+      ovmf.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
+      swtpm.enable = true;
+      runAsRoot = false;
+    };
+  };
+
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
+
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+    };
+  };  
+  
   # enabling services
-  virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
   # services.openssh.enable = true;
 
